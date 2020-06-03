554d33c0a4483abcfdc1cc8c594bb79810171d07
omunizb <oriolmuniz@gmail.com>
2020-06-03 10:55:30 +0200

    petita correcció

--- usflights.sql
+++ usflights.sql
@@ -3,5 +3,5 @@ SELECT Origin, AVG(ArrDelay) AS prom_arribades, AVG(DepDelay) AS prom_sortides F
 SELECT origin, colYear, colMonth, AVG(ArrDelay) AS prom_arribades FROM flights GROUP BY origin, colYear, colMonth ORDER BY origin, colYear, colMonth;
 SELECT City, colYear, colMonth, AVG(ArrDelay) AS prom_arribades FROM flights INNER JOIN usairports ON flights.Origin = usairports.IATA GROUP BY City, colYear, colMonth ORDER BY City, colYear, colMonth;
 SELECT UniqueCarrier, colYear, colMonth, SUM(Cancelled) AS total_cancelled FROM flights GROUP BY UniqueCarrier, colYear, colMonth ORDER BY total_cancelled DESC;
-SELECT TailNum, SUM(Distance) AS totalDistance FROM flights WHERE TailNum <> "" GROUP BY TailNum ORDER BY totalDistance DESC LIMIT 10;
+SELECT TailNum, SUM(Distance) AS totalDistance FROM flights WHERE TailNum != "" GROUP BY TailNum ORDER BY totalDistance DESC LIMIT 10;
 SELECT UniqueCarrier, AVG(ArrDelay) AS avgDelay FROM flights GROUP BY UniqueCarrier HAVING avgDelay > 10 ORDER BY avgDelay DESC; 
